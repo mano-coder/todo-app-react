c0db5d00a87c15d5316116bcf06fa419d1c92d8f
mano-coder <bis.anouar5@gmail.com>
2026-02-26 16:25:19 +0000

    feat: drag tasks around

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,9 +1,31 @@
 import iconcross from "../images/icon-cross.svg";
 
-export default function taskslist({ tasks, deleteTask, toggleCheckBox }) {
+export default function taskslist({ tasks, deleteTask, toggleCheckBox, setTasks }) {
   const maptasks = tasks.map(({ text, completed, id }) => {
     return (
-      <li className="box" draggable="true" key={id}>
+      <li
+        className="box"
+        draggable="true"
+        key={id}
+        data-id={id}
+        onDragStart={(e) => {
+          const target = e.target.closest("li");
+          if (target) {
+            target.classList.add("dragging");
+          }
+        }}
+        onDragEnd={(e) => {
+          const target = e.target.closest("li");
+          if (target) {
+            target.classList.remove("dragging");
+          }
+          const newOrder = [...document.querySelectorAll("#task-list li")].map(
+            (el) => el.dataset.id,
+          );
+          console.log(newOrder);
+          setTasks(newOrder.map((id) => tasks.find((task) => task.id === id)));
+        }}
+      >
         <span
           className={`checkbox ${completed ? "checked" : ""}`}
           onClick={() => toggleCheckBox(id)}
@@ -17,5 +39,25 @@ export default function taskslist({ tasks, deleteTask, toggleCheckBox }) {
       </li>
     );
   });
-  return <ul id="task-list">{maptasks}</ul>;
+  return (
+    <ul
+      id="task-list"
+      onDragOver={(e) => {
+        e.preventDefault(); // Necessary to allow a drop
+        const draggingItem = document.querySelector(".dragging");
+        const siblings = [
+          ...e.currentTarget.querySelectorAll("li:not(.dragging)"),
+        ];
+
+        // Find the sibling that the dragging item should be placed before
+        let nextSibling = siblings.find((sibling) => {
+          return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+        });
+
+        e.currentTarget.insertBefore(draggingItem, nextSibling);
+      }}
+    >
+      {maptasks}
+    </ul>
+  );
 }
