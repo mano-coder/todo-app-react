4bffc66ebda42b7563f3112afaeace8e1bbc2b6b
mano-coder <bis.anouar5@gmail.com>
2026-02-26 16:24:38 +0000

    feat: clear completed tasks button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,29 @@
-export default function Footer({ itemsLeft }) {
+export default function Footer({ itemsLeft, setFilter, filter, clearCompleted }) {
   return (
     <footer className="box">
       <span id="item-left">{itemsLeft < 0 ? "-" : itemsLeft()} items left</span>
       <div className="filter-options">
-        <button type="button" className="selected">
+        <button
+          type="button"
+          className={filter === "all" ? "selected" : ""}
+          onClick={() => setFilter("all")}
+        >
           All
         </button>
-        <button type="button">Active</button>
-        <button type="button">Completed</button>
+        <button
+          type="button"
+          className={filter === "active" ? "selected" : ""}
+          onClick={() => setFilter("active")}
+        >
+          Active
+        </button>
+        <button
+          type="button"
+          className={filter === "completed" ? "selected" : ""}
+          onClick={() => setFilter("completed")}
+        >
+          Completed
+        </button>
       </div>
       <button type="button" id="clear-completed" onClick={() => clearCompleted()}>
         Clear Completed
